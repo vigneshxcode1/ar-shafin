217994bd18ef19de3e8e5b270fd550dd7a7c97c5
vigneshxcode1 <vigneshxcode@gmail.com>
2024-12-25 16:49:24 +0530

    last commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://ar-shafin-server.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'admin-id','Content-Type,Authorization']
   

1d23e1bfd3090d1c5219b8ceadf948cc20acdbc8
vigneshxcode1 <vigneshxcode@gmail.com>
2025-01-02 16:09:44 +0530

    last  commit

--- frontend/src/componets/admin/Updatemosques.jsx
+++ frontend/src/componets/admin/Updatemosques.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-
+import './Updatemosque.css'
 
 const BASE_URL = "https://ar-shafin-server.onrender.com";
 
